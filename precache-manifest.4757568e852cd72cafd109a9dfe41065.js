94b638a0a5e2de91ad28fab025fe3da4bae91895
xAlien95 <xalien95@live.it>
2019-03-18 21:10:45 +0100

    Updates

---
+++ precache-manifest.4757568e852cd72cafd109a9dfe41065.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29ef059957ab7c7655ef",
+    "url": "/shortcut-preview/static/js/runtime~main.eabeac8d.js"
+  },
+  {
+    "revision": "43868836c2705c168f2c",
+    "url": "/shortcut-preview/static/js/main.8af04269.chunk.js"
+  },
+  {
+    "revision": "c96a6475d3203f6c52ae",
+    "url": "/shortcut-preview/static/js/2.d60f193c.chunk.js"
+  },
+  {
+    "revision": "43868836c2705c168f2c",
+    "url": "/shortcut-preview/static/css/main.95fbae0a.chunk.css"
+  },
+  {
+    "revision": "60c341a1d6da5627edd7d519c051726c",
+    "url": "/shortcut-preview/index.html"
+  }
+];
